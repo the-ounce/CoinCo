96b50275ac8cb413f39c1faad9882ba52cc1c1bd
Mykyta Havrylenko <gavverment@gmail.com>
2022-06-23 21:07:13 +0300

    Design updates (UIPicker); Refactor of CryptoVC

--- Bitco/Controller/CryptoViewController.swift
+++ Bitco/Controller/CryptoViewController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-class CryptoViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CryptoManagerDelegate {
-
+class CryptoViewController: UIViewController {
+    
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
@@ -22,16 +22,33 @@ class CryptoViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         currencyPicker.delegate = self
     }
     
+    
+}
+
+//MARK: - UIPickerView
+
+extension CryptoViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return cryptoManager.currencyArray.count
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50.0
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return cryptoManager.currencyArray[row]
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        var label = UILabel()
+        if let v = view as? UILabel { label = v }
+        label.font = UIFont (name: "Avenir-Heavy", size: 25)
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        label.text =  cryptoManager.currencyArray[row]
+        label.textAlignment = .center
+        return label
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return cryptoManager.currencyArray.count
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
@@ -39,18 +56,21 @@ class CryptoViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         cryptoManager.getCoinPrice(for: selectedCurrency)
     }
     
+}
+
+//MARK: - CryptoManagerDelegate
+
+extension CryptoViewController: CryptoManagerDelegate {
+    
     func didUpdateRateInfo(_ cryptoRateInfo: CryptoModel) {
-        
         DispatchQueue.main.async {
             self.priceLabel.text = cryptoRateInfo.stringPrice
             self.currencyLabel.text = cryptoRateInfo.currency
         }
-        
     }
     
     func didFailWithError(error: Error) {
         print(error)
     }
-
 }
 
